27973a09084cfc6b5fd40c17215d9de24c91efdb
Hung Le <hle@webroot.com>
2020-02-01 12:22:55 -0700

    Adding more NumPy methods for matrix

--- numpy_methods.py
+++ numpy_methods.py
@@ -83,3 +83,82 @@ print(matrix[:2, :])
 # Select all rows and second column of the matrix
 print(matrix[:, 1:2])
 
+# Checking sizes of matrix
+matrix = np.array([
+    [1, 2, 3, 4],
+    [5, 6, 7, 8],
+    [9, 10, 11, 12]
+])
+
+# View the number of rows and columns
+print(matrix.shape)
+
+# View the number of elements (rows * columns
+print(matrix.size)
+
+# View the number of dimensions
+print(matrix.ndim)
+
+# Vectorize function
+matrix = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+# Create a lambda function that adds 100 to something
+add_100 = lambda i: i + 100
+
+# Create a vectorized function
+vectorized_add_100 = np.vectorize(add_100)
+
+print(vectorized_add_100(matrix))
+
+# Or broadcasting will do the same
+print(matrix + 100)
+
+# Returns the maximum element in the matrix
+print(np.max(matrix))
+
+# And the minimum
+print(np.min(matrix))
+
+# Returns the maximum element in each column
+print(np.max(matrix, axis=0))
+
+# and the maximum element for each row
+print(np.max(matrix, axis=1))
+
+# Calculate the mean value of a matrix
+print(np.mean(matrix))
+
+# Return the variance
+print(np.var(matrix))
+
+# Return the standard deviation
+print(np.std(matrix))
+
+# Find the mean value in each column
+print(np.mean(matrix, axis=0))
+
+# Reshaping Arrays
+# Adding a row to the existing matrix
+matrix = np.array([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9],
+    [10, 11, 12]
+])
+print(matrix.reshape(2, 6))
+# Reshape with 1 row, and "as many as needed" columns
+print(matrix.reshape(1, -1))
+# Reshape with 1 column, and "as many as needed" rows
+print(matrix.reshape(-1, 1))
+
+# Transposing a matrix
+print(matrix.T)
+
+
+
+
+
+
